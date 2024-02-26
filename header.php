5d76b4cee20492f4383784c2debe64b8f6f0c8ce
Sadia-Aumie <sadiaaumie@gmail.com>
2024-02-26 21:11:48 +0600

    update

---
+++ header.php
@@ -0,0 +1,15 @@
+<section class="header">
+
+   <a href="home.php" class="logo">Wedding.</a>
+
+   <nav class="navbar">
+      <a href="http://127.0.0.1:5500/new%20pro/app.html">home</a>
+      <a href="about.php">about</a>
+      <a href="http://localhost/ORDERONLINE/index.php?page=home">portfolio</a>
+      <a href="pricing.php">pricing</a>
+      <a href="contact.php">contact</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
